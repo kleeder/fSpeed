6a3aaf89e8e32a25324b97f99365325accb1f835
kleeder <5kleeman@informatik.uni-hamburg.de>
2019-07-23 06:58:11 +0200

    first solution works better lol

--- fSpeed.py
+++ fSpeed.py
@@ -1,55 +1,42 @@
 import scipy.io.wavfile
 
 rate, data = scipy.io.wavfile.read('music.wav')
-data_min = 0
-data_max = 0
-for i in range(len(data)):
-    if data[i] < data_min:
-        data_min = data[i]
-    if data[i] > data_max:
-        data_max = data[i]
 
-if data_max < abs(data_min):
-    biggestValue = abs(data_min)
-else:
-    biggestValue = data_max
-
-bigF = biggestValue // 15
 file = open('musictest.fss', 'w')
 file.write("0.5\n")
 for i in range(len(data)):
     data_new = abs(data[i])
     if data_new == 0:
         file.write("c4f0\n")
-    elif 0 < data_new <= bigF:
+    elif 0 < data_new <= 1000:
         file.write("c4f1\n")
-    elif bigF < data_new <= bigF*2:
+    elif 1000 < data_new <= 1500:
         file.write("c4f2\n")
-    elif bigF*2 < data_new <= bigF*3:
+    elif 1500 < data_new <= 2000:
         file.write("c4f3\n")
-    elif bigF*3 < data_new <= bigF*4:
+    elif 2000 < data_new <= 2500:
         file.write("c4f4\n")
-    elif bigF*4 < data_new <= bigF*5:
+    elif 2500 < data_new <= 3000:
         file.write("c4f5\n")
-    elif bigF*5 < data_new <= bigF*6:
+    elif 3000 < data_new <= 3500:
         file.write("c4f6\n")
-    elif bigF*6 < data_new <= bigF*7:
+    elif 3500 < data_new <= 4000:
         file.write("c4f7\n")
-    elif bigF*7 < data_new <= bigF*8:
+    elif 4000 < data_new <= 5000:
         file.write("c4f8\n")
-    elif bigF*8 < data_new <= bigF*9:
+    elif 5000 < data_new <= 6000:
         file.write("c4f9\n")
-    elif bigF*9 < data_new <= bigF*10:
+    elif 6000 < data_new <= 7500:
         file.write("c4fa\n")
-    elif bigF*10 < data_new <= bigF*11:
+    elif 7500 < data_new <= 10000:
         file.write("c4fb\n")
-    elif bigF*11 < data_new <= bigF*12:
+    elif 10000 < data_new <= 12500:
         file.write("c4fc\n")
-    elif bigF*12 < data_new <= bigF*13:
+    elif 12500 < data_new <= 15000:
         file.write("c4fd\n")
-    elif bigF*13 < data_new <= bigF*14:
+    elif 15000 < data_new <= 20000:
         file.write("c4fe\n")
-    elif bigF*14 < data_new:
+    elif 20000 < data_new <= 30000:
         file.write("c4ff\n")
     #print(data_new)
 file.close()
